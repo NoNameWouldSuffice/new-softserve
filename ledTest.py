5f494a796bcd28ed8511c2c8b1cb38fb1fc8c62a
Lachlan Paulsen <fireballinator@gmail.com>
2020-12-14 16:44:33 +1300

    Tested with external files

---
+++ ledTest.py
@@ -0,0 +1,33 @@
+from softservetbn import *
+from random import randint
+import time
+
+jb1 = Jackbord("red.head", "111254186336836811343", "7b3b518e98")
+
+total_leds = 4
+pin_no = "a2"
+led_no = 0
+red = 0
+green = 0
+blue = 0
+
+print("Smart LED Program Started")
+jb1.cmd("sled {0} {1} {2} {3} {4} {5}".format(pin_no, total_leds, led_no, red, green, blue))
+
+while True:
+    red = randint(0, 255)
+    green = randint(0, 255)
+    blue = randint(0, 255)
+
+
+
+    #Variables are difficult to put into python command.
+    #They would need to be string formatted into the command
+    jb1.cmd("sled {0} {1} {2} {3} {4} {5}".format(pin_no, total_leds, led_no, red, green, blue))
+
+    if led_no >= total_leds:
+        led_no = 0
+    else:
+        led_no += 1
+
+    time.sleep(0.05)
--- softservetbn.py
+++ softservetbn.py
@@ -1,4 +1,6 @@
 import paho.mqtt.client as mqtt
+from queue import Queue
+import time
 
 #TODO / THOUGHTS / possible features:
 #   Type enforcing: Give the user the ability to specify what data-type the variable will be automagically interpreted as
@@ -21,7 +23,6 @@ class Jackbord():
         self.__channelClassList = {} #Format: "mqtt topic" : ChannelClass instance
 
         self.hostAddress = "mqtta.jackbord.org"
-        print(type(self.hostAddress))
 
         self.__jackbordID = jackbordID
 
@@ -30,6 +31,12 @@ class Jackbord():
         self.__mqttClient = mqtt.Client()
 
         self.__openMqttServer(username, password)
+
+        self.__inLiveMode = False
+        self.__printOutput = ""
+
+
+        
     
 
     def __openMqttServer(self, username, password):
@@ -44,25 +51,32 @@ class Jackbord():
 
 
     def __onMqttConnect(self, client, userdata, flags, rc):
-            print("Connected to Mqtt server with result code ", (rc))
+            #print("Connected to Mqtt server with result code ", (rc))
+            pass
     
     def __onMqttMessage(self, client, userdata, message):
+        
+        if message.topic == str(self.__jackbordID + "/jprint") and self.__inLiveMode == True:
+            self.__printOutput += (message.payload.decode() + "\n")
+        
         if (message.topic) in self.__channelClassList.keys():
             incomingValue = message.payload.decode()
 
             if incomingValue != self.__channelClassList[message.topic].get():
                 self.__channelClassList[message.topic].updateFromServer(incomingValue)
+        
+
     
-    def bindVar(self, topic):
+    def bindChan(self, channelNum):
         
         #Enforce string type on topic key
-        if not type(topic) == str:
-            topic = str(topic)
+        if not type(channelNum) == str:
+            channelNum = str(channelNum)
 
         #String parsing0.
-        newChannelClass = channel(self.__mqttClient, self.__jackbordID, topic)
-        self.__channelClassList[str(self.__jackbordID + "/" + topic)] = newChannelClass
-        self.__mqttClient.subscribe(self.__jackbordID + "/" + topic)
+        newChannelClass = channel(self.__mqttClient, self.__jackbordID, channelNum)
+        self.__channelClassList[str(self.__jackbordID + "/chan/" + channelNum)] = newChannelClass
+        self.__mqttClient.subscribe(self.__jackbordID + "/chan/" + channelNum)
 
         return(newChannelClass)
     
@@ -70,28 +84,42 @@ class Jackbord():
         self.__mqttClient.publish(str(self.__jackbordID + "/cmd"), payload=commandString)
     
     def cmdlive(self):
+        self.__inLiveMode = True
+        self.__mqttClient.subscribe(str(self.__jackbordID + "/jprint"))
         print("This is live command mode for the not softserve:")
         print("Please hit Ctrl + C to return to python interpreter")
         print("Yes, this is quite developmental. Yes, this is really only if you are in python interactive shell")
 
         while True:
-            command = input("JB CMD>>")
-            self.cmd(command)
+            try:
+                time.sleep(1)
+                if not self.__printOutput == "":
+                    print(self.__printOutput)
+                    self.__printOutput = ""
+
+                
+                command = input("JB CMD>>")
+                self.cmd(command)
+                    
+            except KeyboardInterrupt:
+                self.__inLiveMode = False
+                self.__mqttClient.unsubscribe(str(self.__jackbordID + "/jprint"))
+                print()
+                break
 
 
 
 
 
 class channel():
-    def __init__(self, mqttClient, jackbordID, topic):
+    def __init__(self, mqttClient, jackbordID, channelNum):
         self.__mqttClient = mqttClient
         self.jackbordID = jackbordID
-        self.__topic = topic
+        self.__channelNum = channelNum
         self.__value = "Null"
 
     def updateFromServer (self, message):
         self.__value = message
-        print("Message updated")
 
     
     def get(self):
@@ -99,4 +127,8 @@ class channel():
     
     def set(self, value):
         self.__value = value
-        self.__mqttClient.publish(topic=str(self.jackbordID + "/" + self.__topic), payload=value)
+        self.__mqttClient.publish(topic=str(self.jackbordID + "/set/" + self.__channelNum), payload=value)
+
+
+if __name__ == "__main__":
+    jb1 = Jackbord("red.head", "111254186336836811343", "7b3b518e98")
