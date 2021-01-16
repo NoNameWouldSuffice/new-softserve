2f4dd7f4a75d2fb01d31aff64020e910d44a236e
Lachlan Paulsen <32665702+NoNameWouldSuffice@users.noreply.github.com>
2021-01-16 15:40:30 +1300

    Delete softserve-test.py

--- softserve-test.py
+++
@@ -1,7 +0,0 @@
-import softserve
-import time
-
-#jb1 = softserve.Jackbord("mae.stro","116118993476090577725","1085e4b555")
-
-jb1 = softserve.Jackbord("red.head", "111254186336836811343", "1fa60c9fc6")
-jb1.cmd("d1 1")
